c428295de6a22c6e9674e849e7759458e37d7006
num5268 <num5268@gmail.com>
2021-07-01 16:53:09 +0900

    num5268

--- Sport/src/main/java/com/team/sport/controller/SearchController.java
+++ Sport/src/main/java/com/team/sport/controller/SearchController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.slf4j.Slf4j;
-
+/*
+*
+*/
 @Controller
 @Slf4j
 @RequestMapping(value="/search")
